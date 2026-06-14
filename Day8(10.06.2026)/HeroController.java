9445a228b8b1cca3bf174cadacda3f5463bd93fe
Sudeshna-13 <sude13shna@gmail.com>
2026-06-14 20:35:58 +0530

    Add files via upload

---
+++ Day8(10.06.2026)/HeroController.java
@@ -0,0 +1,19 @@
+package com.example.hero;
+import org.springframework.web.bind.annotation.*;
+import java.util.ArrayList;
+import java.util.List;
+@RestController
+@RequestMapping("/heroes")
+public class HeroController {
+    private List<Hero> heroes = new ArrayList<>();
+    @GetMapping
+    public List <Hero> getHeroes()
+    {
+        return heroes;
+    }
+    @PostMapping
+    public Hero addHero(@RequestBody Hero hero){
+        heroes.add(hero);
+        return hero;
+    }
+}
